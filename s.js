bdeb00ca0aeeb7602cfd622b6704923c4c564257
Neha Sharma <86730309+Neha120903@users.noreply.github.com>
2022-12-07 23:00:51 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,7 @@
+
+const adderBtn = document.getElementById('add-btn')
+document.getElementById('get-ticket').style.display = "none";
+adderBtn.addEventListener('click',() => {
+    document.getElementById('get-ticket').style.display = "";
+
+})
